e769498df6f9dda1d57849bec5e220ae7d413666
Tony Trinh <tony19@gmail.com>
2021-03-12 20:38:31 -0600

    demo: assets/css/variables.scss

--- src/App.vue
+++ src/App.vue
@@ -58,3 +58,7 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss">
+@import './app.scss'
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "~@/assets/css/variables.scss"`
+      },
+      // by default the `sass` option will apply to both syntaxes
+      // because `scss` syntax is also processed by sass-loader underlyingly
+      // but when configuring the `prependData` option
+      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
+      // in that case, we can target the `scss` syntax separately using the `scss` option
+      scss: {
+        additionalData: `@import "~@/assets/css/variables.scss";`
+      },
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
